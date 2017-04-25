99ddf2019242675c706caab5c2654a565b1267fd
Thomas Weiss <tweiss@octo.com>
2017-04-25 15:49:09 +0200

    add domain email authorized

--- controllers/verifydomains.js
+++ controllers/verifydomains.js
@@ -5,27 +5,50 @@ const express = require('express'),
 const sanitizeHtml = require('sanitize-html');
 //allow verifications and operations on email strings
 const validator = require('validator');
-
+const fs = require('fs');
+const parse = require('csv-parse');
+//http://csv.adaltas.com/parse/examples/
 const verify = function (data, callback) {
   if (data) {
 	  const email = validator.normalizeEmail(data);
 	  const emailValid = validator.isEmail(email);
-	  const domainArray = [".(gouv)\.(fr)$", "(octo)\.(com)$"];
-	  const re = new RegExp(domainArray.join("|"), "i");
-      const obj = {};
-      if (emailValid && !email.match(re) )  {
-		  obj.success = false;
-		  obj.msg  = 'Votre email n\'est pas autorisé pour déployer une consultation.';
-	  } else {
-	  	  obj.success = true;
-	  }
-      callback(obj);
+	  fs.readFile('./domain_consultation.csv', 'utf8', function (err,data) {
+		  if (err) {
+			  return console.log(err);
+		  }
+
+		  parse(data,{delimiter: ","}, function(err, output){
+              if (err) {
+                  return console.log(err);
+              }
+
+              const re = new RegExp(output[0].join("|"), "i");
+
+              const obj = {};
+              if (!emailValid)  {
+				      obj.success = false;
+				      obj.status = 500;
+				      obj.msg = 'Votre email est mal formé.';
+              }
+              else if (emailValid && !email.match(re) )  {
+					  obj.success = false;
+					  obj.status = 400;
+					  obj.msg  = 'Votre email n\'est pas autorisé pour déployer une consultation.';
+			  } else {
+				      obj.status= 200;
+					  obj.success = true;
+			  }
+			 // console.log(obj);
+              callback(obj);
+          });
+
+	  });
   }
 }
 
 router.post('/verify', function(req, res, next) {
  verify(req.body.adminEmail, function(data) {
-	 return res.json(data);
+	 return res.status(data.status).json(data);
  });
 });
 
