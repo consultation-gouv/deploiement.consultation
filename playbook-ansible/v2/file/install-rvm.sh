063f95d881d4722f46e31e1b2c25018c4a87a857
Sébastien Delafond <sdelafond@gmail.com>
2018-11-13 12:35:53 +0100

    Use ruby 2.3.1 via RVM
    https://github.com/consultation-gouv/consultation.gouv.fr/commit/daec5c025995c83d5ab58b4ebea6eb9a824958f7#commitcomment-31279020

--- playbook-ansible/v2/file/install-rvm.sh
+++ playbook-ansible/v2/file/install-rvm.sh
@@ -4,4 +4,4 @@ gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB8
 curl -sSL https://get.rvm.io | bash -s stable
 source /usr/local/rvm/scripts/rvm
 echo "source /usr/local/rvm/scripts/rvm" >> ~/.bashrc
-rvm install 2.2.3
+rvm install 2.3.1
